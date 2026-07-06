15aa08b9d4a6f5da1b62958c4408fd6e774ae001
Eyood <ulfchh404@gmail.com>
2026-07-06 09:21:41 +0700

    Create [1]menghitung-luas-persegi-menggunakan-panjang-dan-lebar-persegi.py
    Latihan pertama selesai

---
+++ Python/brocodez-course/[1]menghitung-luas-persegi-menggunakan-panjang-dan-lebar-persegi.py
@@ -0,0 +1,7 @@
+#ini adalah kode python yang kubuat untuk menghitung luas dari sebuah persegi panjang menggunakan panjang dan lebar daei persegi panjang
+print("Silahkan input panjang dan lebar persegi panjang yang ingin anda hitung luasnya di bawah ini! (Pastikan input berupa angka)") #memberikan sebuah arahan kepada user
+panjang = float(input("panjang: ")) #mengambil input variable panjang dari user lalu mengubahnya menjadi tipe float agar bisa di operasikan menggunakan fungsi matematika dan lebih presisi dibandingkan menggunakan tipe int
+lebar = float(input("lebar: ")) #sama kaya yang diatas, ngambil input user versi variable lebar
+luas = panjang * lebar #membuat variable luas menggunakan panjang dan lebar yang telah diinput oleh user sebelumnya
+print(f"luas persegi panjang tersebut adalah {luas}") #menggunakan formatted string, memberikan output variable luas yang telah dihitung kepada user
+#terimakasih sudah melihat kode python pertama ku, jadi intinya ini variable a dikali vaeiable b doang sih cuma dibuat lebih keren aja pake nama menghitung-luas-persegi-menggunakan-panjang-dan-lebar-persegi.py btw ini exercise no 1 dari brocode python 12 jam itu, see you later, bye.
